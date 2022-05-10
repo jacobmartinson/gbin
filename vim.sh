808fd5c3001b60796aff24afc870e24b3f0b6e88
Jacob <jacobm3@gmail.com>
2022-05-10 15:10:09 -0500

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -18,41 +18,36 @@ if exists("syntax_on")
   syntax reset
 endif
 let colors_name = "jacobm3"
-hi Normal ctermbg=Black ctermfg=LightGrey
-hi ErrorMsg         	term=standout   ctermbg=DarkRed ctermfg=White
-hi IncSearch        	term=reverse        	cterm=bold
-hi ModeMsg                  	term=bold                   	cterm=bold
-hi StatusLine   term=bold                   	cterm=bold
-hi StatusLineNC term=bold                   	cterm=bold
-hi VertSplit        	term=bold                   	cterm=bold
-hi Visual                   	term=bold                   	cterm=reverse
-hi VisualNOS        	term=underline,bold cterm=underline,bold
-hi DiffText         	term=reverse cterm=bold ctermbg=grey
-hi Directory        	term=bold ctermfg=cyan
-hi LineNr                   	term=underline ctermfg=darkgrey
-hi MoreMsg                  	term=bold ctermfg=LightGreen
-hi NonText                  	term=bold ctermfg=darkgrey
-hi Question         	term=standout ctermfg=LightGreen
-hi Search                   	term=reverse ctermbg=Yellow ctermfg=Black
-hi SpecialKey   term=bold ctermfg=grey
-hi Title                            	term=bold ctermfg=darkGreen
+hi Normal ctermbg=233 ctermfg=253
+hi ErrorMsg         term=standout   ctermbg=DarkRed ctermfg=White
+hi IncSearch        term=reverse        cterm=bold
+hi StatusLine   term=bold                   cterm=bold
+hi StatusLineNC term=bold                   cterm=bold
+hi VertSplit        term=bold                   cterm=bold
+hi Visual                   term=bold                   cterm=reverse
+hi VisualNOS        term=underline,bold cterm=underline,bold
+hi DiffText         term=reverse cterm=bold ctermbg=grey
+hi Directory        term=bold ctermfg=yellow
+hi LineNr                   term=underline ctermfg=darkgrey
+hi Question         term=standout ctermfg=LightGreen
+hi Search                   term=reverse ctermbg=Yellow ctermfg=Black
 hi WarningMsg   term=standout ctermfg=grey
-hi WildMenu                 	term=standout ctermbg=Yellow ctermfg=Black
-hi Folded                   	term=standout ctermbg=LightGrey ctermfg=grey
+hi WildMenu                 term=standout ctermbg=Yellow ctermfg=Black
+hi Folded                   term=standout ctermbg=LightGrey ctermfg=grey
 hi FoldColumn   term=standout ctermbg=LightGrey ctermfg=grey
-hi DiffAdd                  	term=bold ctermbg=grey
-hi DiffChange   term=bold ctermbg=cyan
-hi DiffDelete   term=bold ctermfg=grey ctermbg=cyan
-hi Comment                    	ctermfg=darkgrey
-hi String                            	ctermfg=grey
-hi Statement         	ctermfg=darkblue
-hi Keyword           	ctermfg=darkblue
-hi PreCondit         	ctermfg=darkblue
-hi Function          	ctermfg=grey
-hi Constant         	term=underline ctermfg=grey
-hi Special                  	term=bold ctermfg=grey
+hi Comment           ctermfg=246
+hi String            ctermfg=081
+hi Statement         ctermfg=119
+hi Keyword           ctermfg=21
+hi Include           ctermfg=21
+hi PreCondit         ctermfg=21
+hi Function          ctermfg=165
+hi Constant         ctermfg=21
+hi Define         ctermfg=165
+hi Special          ctermfg=21
 if &t_Co > 8
-  hi Statement  term=bold cterm=bold ctermfg=grey
+  hi Statement  ctermfg=21
 endif
-hi Ignore                   	ctermfg=LightGrey
+hi Ignore                   ctermfg=LightGrey
+
 EOF
